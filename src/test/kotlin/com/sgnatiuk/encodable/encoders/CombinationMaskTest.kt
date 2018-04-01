131f90d95a2832ae1bc995eb864150e7b742812a
Sergiy Gnatyuk <surpsg2012@gmail.com>
2018-04-01 21:43:58 +0300

    refactoring

--- src/test/kotlin/com/sgnatiuk/encodable/encoders/CombinationMaskTest.kt
+++ src/test/kotlin/com/sgnatiuk/encodable/encoders/CombinationMaskTest.kt
@@ -1,22 +1,17 @@
 package com.sgnatiuk.encodable.encoders
 
+import com.sgnatiuk.encodable.TestData
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertTrue
 import org.junit.Test
 
 internal class CombinationMaskTest {
-    private val itemsCount = 3
-    private val bases = IntArray(itemsCount) { it + 1 }
-    private val expectedCombinations = bases.let { baseItem ->
-        var res = 1
-        baseItem.forEach { res *= it }
-        res
-    }
+    private val testData = TestData(3)
 
     @Test
     fun `verify hasNext can be called multiple times`(){
-        val combinationMask = CombinationMask(bases)
-        repeat(expectedCombinations){
+        val combinationMask = CombinationMask(testData.bases)
+        repeat(testData.expectedCombinations){
             val hasNext = combinationMask.hasNext() && combinationMask.hasNext()
             assertTrue(hasNext)
             combinationMask.next()
@@ -26,9 +21,9 @@ internal class CombinationMaskTest {
     @Test
     fun `verify all possible combinations are generated`() {
         val combinationSet = HashSet<List<Int>>()
-        CombinationMask(bases).forEach {
+        CombinationMask(testData.bases).forEach {
             combinationSet.add(it.toList())
         }
-        assertEquals(expectedCombinations, combinationSet.size)
+        assertEquals(testData.expectedCombinations, combinationSet.size)
     }
 }
