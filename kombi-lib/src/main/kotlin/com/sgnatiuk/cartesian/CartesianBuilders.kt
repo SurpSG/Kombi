08e62118849a44da326c3d80c39b1aabc11e5fad
Sergiy Gnatyuk <surpsg2012@gmail.com>
2018-04-09 20:54:01 +0300

    added bintray upload plugin

--- kombi-lib/src/main/kotlin/com/sgnatiuk/cartesian/CartesianBuilders.kt
+++ kombi-lib/src/main/kotlin/com/sgnatiuk/cartesian/CartesianBuilders.kt
@@ -5,14 +5,10 @@ package com.sgnatiuk.cartesian
 fun <K, V> CartesianProduct(
         map: Map<K, Collection<V>>,
         keepOrder: Boolean = false
-) : CartesianProduct<Map<K, V>>{
-    return CartesianProductMap(map, keepOrder)
-}
+) : CartesianProduct<Map<K, V>> = CartesianProductMap(map, keepOrder)
 
 @JvmOverloads
 fun <T> CartesianProduct(
     collection : Collection<Collection<T>>,
     keepOrder: Boolean = false
-) : CartesianProduct<List<T>> {
-    return CartesianProductSet(collection, keepOrder)
-}
+) : CartesianProduct<List<T>> = CartesianProductSet(collection, keepOrder)
