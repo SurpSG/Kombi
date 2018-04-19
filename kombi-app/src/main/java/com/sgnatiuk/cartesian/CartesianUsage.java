42ee10c724fee4771299f267d56e7056b41f2495
Sergii Gnatiuk <surpsg2012@gmail.com>
2018-04-19 23:53:40 +0300

    updated usage example java

--- kombi-app/src/main/java/com/sgnatiuk/cartesian/CartesianUsage.java
+++ kombi-app/src/main/java/com/sgnatiuk/cartesian/CartesianUsage.java
@@ -1,22 +1,21 @@
 package com.sgnatiuk.cartesian;
 
-import java.util.Arrays;
+import com.sgnatiuk.TestData;
+
 import java.util.List;
 import java.util.Map;
-import java.util.stream.IntStream;
-
-import static java.util.stream.Collectors.toMap;
+import java.util.stream.Collectors;
 
 public class CartesianUsage {
 
     public static void main(String[] args) {
-        List<List<Integer>> inputData = Arrays.asList(
-                Arrays.asList(1, 2, 3),
-                Arrays.asList(4),
-                Arrays.asList(5, 6)
-        );
-        printCartesianProductFromList(inputData);
-        printCartesianProductFromMap(inputData);
+        printCartesianProductFromList(TestData.getListOfLists());
+        printCartesianProductFromMap(TestData.getMapOfLists());
+
+        System.out.println();
+
+        parallelPrintCartesianProduct(TestData.getListOfLists());
+        parallelPrintCartesianProduct(TestData.getMapOfLists());
     }
 
     public static <T> void printCartesianProductFromList(List<List<T>> inputData) {
@@ -25,13 +24,37 @@ public class CartesianUsage {
                 .forEach(System.out::println);
     }
 
-    public static <T> void printCartesianProductFromMap(List<List<T>> inputData) {
-        Map<Integer, List<T>> inputDataMap = IntStream.range(0, inputData.size())
-                .boxed().collect(
-                        toMap(i -> i, inputData::get)
-                );
-        System.out.println("Cartesian product of " + inputDataMap);
-        CartesianBuilder.cartesianProductOf(inputDataMap)
+    public static <K, V> void printCartesianProductFromMap(Map<K, List<V>> inputData) {
+        System.out.println("Cartesian product of " + inputData);
+        CartesianBuilder.cartesianProductOf(inputData)
                 .forEach(System.out::println);
+    }
+
+    public static <T> void parallelPrintCartesianProduct(List<List<T>> inputData) {
+        System.out.println("Split Cartesian product of " + inputData);
+        parallelPrintCartesianProduct(CartesianBuilder.cartesianProductOf(inputData));
+    }
+
+    public static <K, V> void parallelPrintCartesianProduct(Map<K, List<V>> inputData) {
+        System.out.println("Split Cartesian product of " + inputData);
+        parallelPrintCartesianProduct(CartesianBuilder.cartesianProductOf(inputData));
+    }
+
+    public static <T> void parallelPrintCartesianProduct(CartesianProduct<T> cartesianProduct) {
+        List<Thread> threadList = cartesianProduct.split(2).stream()
+                .map(subProduct -> (Runnable) () ->
+                        subProduct.forEach(System.out::println)
+                ).map(Thread::new).collect(Collectors.toList());
+
+        threadList.forEach(Thread::start);
+        threadList.forEach(CartesianUsage::join);
+    }
+
+    public static void join(Thread thread) {
+        try {
+            thread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
