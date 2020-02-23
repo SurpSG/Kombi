a3f6eb04c826e2171f87cc2e6d6545a05a54837a
Sergii Gnatiuk <sergii.gnatiuk@worldapp.com>
2020-02-24 01:53:46 +0200

    updated benchmarks. comparing with guava

--- kombi-jmh/src/jmh/kotlin/com/sgnatiuk/benchmark/cartesian/CartesianListBenchmark.kt
+++ kombi-jmh/src/jmh/kotlin/com/sgnatiuk/benchmark/cartesian/CartesianListBenchmark.kt
@@ -1,34 +1,59 @@
 package com.sgnatiuk.benchmark.cartesian
 
+import com.google.common.collect.Lists
+import com.google.common.collect.Sets
 import com.sgnatiuk.cartesian.CartesianBuilder.cartesianProductOf
-import com.sgnatiuk.cartesian.CartesianProduct
 import org.openjdk.jmh.annotations.*
+import org.openjdk.jmh.infra.Blackhole
 
+@State(Scope.Benchmark)
 open class CartesianListBenchmark {
 
-    @State(Scope.Benchmark)
-    open class SuperState {
-        var result: List<Int>? = null
-        lateinit var cartesianProduct: CartesianProduct<List<Int>>
-        lateinit var iterator: Iterator<List<Int>>
+    @Param("3", "5", "7", "11") 
+    var itemsQuantity: Int = 0
 
-        @Setup(Level.Trial)
-        fun doSetup() {
-            val size = 10
-            val listOfLists = List(size){ i ->
-                List(size - i){ it }
-            }
-            cartesianProduct = cartesianProductOf(listOfLists)
-            iterator = cartesianProduct.iterator()
-        }
+    lateinit var listOfLists: List<Set<Int>>
 
-        @TearDown(Level.Trial)
-        fun doTearDown() {
+    @Setup(Level.Trial)
+    fun doSetup() {
+        listOfLists = List(itemsQuantity) { i ->
+            List(i + 1) { it }.toSet()
         }
     }
 
     @Benchmark
-    fun nextCartesianMap(state: SuperState) {
-        state.result = state.iterator.next()
+    fun cartesianProductList(blackhole: Blackhole) {
+        for (combination in cartesianProductOf(listOfLists, false)) {
+            for (combinationItem in combination) {
+                blackhole.consume(combinationItem)
+            }
+        }
+    }
+
+    @Benchmark
+    fun cartesianProductListKeepOrder(blackhole: Blackhole) {
+        for (combination in cartesianProductOf(listOfLists, true)) {
+            for (combinationItem in combination) {
+                blackhole.consume(combinationItem)
+            }
+        }
+    }
+
+    @Benchmark
+    fun cartesianProductListsGuava(blackhole: Blackhole) {
+        for (combination in Sets.cartesianProduct(listOfLists)) {
+            for (combinationItem in combination) {
+                blackhole.consume(combinationItem)
+            }
+        }
+    }
+
+    @Benchmark
+    fun cartesianProductSetsGuava(blackhole: Blackhole) {
+        for (combination in Lists.cartesianProduct(listOfLists)) {
+            for (combinationItem in combination) {
+                blackhole.consume(combinationItem)
+            }
+        }
     }
 }
